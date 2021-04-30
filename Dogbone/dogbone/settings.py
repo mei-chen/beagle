357f18f330af1327b83e3bbf0b62d34658d1c595
Yixiong Sun <yixiongsun@Maxs-MacBook-Pro.local>
2021-04-30 16:36:31 -0400

    dogbone static directory fix

--- Dogbone/dogbone/settings.py
+++ Dogbone/dogbone/settings.py
@@ -287,7 +287,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.environ.get('STATIC_ASSET_PATH') or ''
+STATIC_ROOT = os.environ.get('STATIC_ASSET_PATH') or 'collectstatic/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
